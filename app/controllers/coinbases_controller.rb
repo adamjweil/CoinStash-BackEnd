efc944de26e7276d5f4929bebd80dc70bb609022
Adam Weil <adamjweil@gmail.com>
2017-10-13 14:50:13 -0400

    coinbase contorller update

--- app/controllers/coinbases_controller.rb
+++ app/controllers/coinbases_controller.rb
@@ -9,7 +9,8 @@ class CoinbasesController < ActionController::API
 
   def accounts
     client
-    render json: @client.accounts
+    # render json: @client.accounts
+    render json: {"id":"2c4d5b15-248e-58f9-b693-c18b84c57375","name":"LTC Wallet","primary":false,"type":"wallet","currency":"LTC","balance":{"amount":"0.00000000","currency":"LTC"},"created_at":"2017-03-31T04:59:24Z","updated_at":"2017-03-31T04:59:25Z","resource":"account","resource_path":"/v2/accounts/2c4d5b15-248e-58f9-b693-c18b84c57375","native_balance":{"amount":"0.00","currency":"USD"}},{"id":"ccf333e9-da73-50bf-9bac-54f324990457","name":"ETH Wallet","primary":false,"type":"wallet","currency":"ETH","balance":{"amount":"0.00000000","currency":"ETH"},"created_at":"2016-08-12T08:56:29Z","updated_at":"2016-09-16T17:00:31Z","resource":"account","resource_path":"/v2/accounts/ccf333e9-da73-50bf-9bac-54f324990457","native_balance":{"amount":"0.00","currency":"USD"}},{"id":"c4b5a004-8131-5a03-a458-6cdc61ab4298","name":"CoinStash USD Test","primary":false,"type":"fiat","currency":"USD","balance":{"amount":"0.00","currency":"USD"},"created_at":"2015-12-26T19:44:25Z","updated_at":"2017-10-10T15:26:36Z","resource":"account","resource_path":"/v2/accounts/c4b5a004-8131-5a03-a458-6cdc61ab4298","native_balance":{"amount":"0.00","currency":"USD"}},{"id":"d81d3213-097e-5b97-ba47-7d88077ab808","name":"BTC Wallet","primary":true,"type":"wallet","currency":"BTC","balance":{"amount":"0.00000000","currency":"BTC"},"created_at":"2015-11-27T07:52:00Z","updated_at":"2017-10-10T16:17:42Z","resource":"account","resource_path":"/v2/accounts/d81d3213-097e-5b97-ba47-7d88077ab808","native_balance":{"amount":"0.00","currency":"USD"}}
   end
 
   def usd_wallet
